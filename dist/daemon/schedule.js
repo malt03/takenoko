35b3c0ecfd9ae25d07f874dd8089c4aac7819726
Koji Murata <koji.murata@dena.com>
2019-08-15 12:37:39 +0900

    build

--- dist/daemon/schedule.js
+++ dist/daemon/schedule.js
@@ -2,12 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const cron_1 = require("cron");
 const vote_1 = require("../vote");
-const logger_1 = require("../logger");
 const notify_1 = require("../notify/notify");
 const hour = Math.floor(Math.random() * 6 + 11); // 11 - 17
 const min = Math.floor(Math.random() * 60);
 const sec = Math.floor(Math.random() * 60);
 const cronTime = `${sec} ${min} ${hour} * * *`;
 notify_1.default(`毎日${hour}時${min}分${sec}秒に投票します`);
-logger_1.default.info('scheduled on ' + cronTime);
 new cron_1.CronJob(cronTime, () => { vote_1.default(); }, undefined, true);
--- dist/vote.js
+++ dist/vote.js
@@ -1,38 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer = require("puppeteer");
-const logger_1 = require("./logger");
 const notify_1 = require("./notify/notify");
 async function default_1() {
-    logger_1.default.info('launching...');
     const browser = await puppeteer.launch();
     try {
         const page = await browser.newPage();
-        logger_1.default.info('loading...');
         await page.goto('https://www.meiji.co.jp/sweets/chocolate/kinotake/cmp/2019senkyo/');
         await page.waitForSelector('.loader', { hidden: true });
         await page.click('.takenoko > .web');
-        logger_1.default.info('clicked...');
         const response = await page.waitForResponse('https://api.vote2019.net/api/vote');
         if (response.ok()) {
             const responseText = await response.text();
             const responseObject = JSON.parse(responseText);
             if (responseObject['status'] && responseObject['status'] === 200) {
                 notify_1.default('たけのこに投票しました！');
-                logger_1.default.info('ok');
             }
             else {
                 notify_1.default('たけのこへの投票に失敗しました… エラー: ' + responseText);
-                logger_1.default.info('error response body: ' + responseText);
             }
         }
         else {
             notify_1.default('たけのこに投票に失敗しました… エラー:' + response.statusText());
-            logger_1.default.info('error response:' + response.statusText());
         }
     }
     catch (err) {
-        logger_1.default.info('error: ' + err);
+        notify_1.default('たけのこに投票に失敗しました… エラー:' + err);
     }
     finally {
         await browser.close();
