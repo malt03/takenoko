2ff2ab2e24281ec1e7a11607841e3fb833d3b508
Koji Murata <koji.murata@dena.com>
2019-08-15 11:31:19 +0900

    remove logger

--- src/vote.ts
+++ src/vote.ts
@@ -1,34 +1,27 @@
 import * as puppeteer from 'puppeteer';
-import logger from './logger';
 import notify from './notify/notify';
 
 export default async function () {
-  logger.info('launching...');
   const browser = await puppeteer.launch();
   try {
     const page = await browser.newPage();
-    logger.info('loading...');
     await page.goto('https://www.meiji.co.jp/sweets/chocolate/kinotake/cmp/2019senkyo/');
     await page.waitForSelector('.loader', { hidden: true });
     await page.click('.takenoko > .web');
-    logger.info('clicked...');
     const response = await page.waitForResponse('https://api.vote2019.net/api/vote');
     if (response.ok()) {
       const responseText = await response.text();
       const responseObject = JSON.parse(responseText);
       if (responseObject['status'] && responseObject['status'] === 200) {
         notify('たけのこに投票しました！');
-        logger.info('ok');
       } else {
         notify('たけのこへの投票に失敗しました… エラー: ' + responseText);
-        logger.info('error response body: ' + responseText);
       }
     } else {
       notify('たけのこに投票に失敗しました… エラー:' + response.statusText());
-      logger.info('error response:' + response.statusText());
     }
   } catch (err) {
-    logger.info('error: ' + err);
+    notify('たけのこに投票に失敗しました… エラー:' + err);
   } finally {
     await browser.close();
   }
