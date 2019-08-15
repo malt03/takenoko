44de51f2d94397fd3370a5e4efe895a6b41d9147
Koji Murata <koji.murata@dena.com>
2019-08-15 11:36:54 +0900

    fix

--- src/daemon/schedule.ts
+++ src/daemon/schedule.ts
@@ -5,6 +5,6 @@ import notify from '../notify/notify';
 const hour = Math.floor(Math.random() * 6 + 11); // 11 - 17
 const min = Math.floor(Math.random() * 60);
 const sec = Math.floor(Math.random() * 60);
-const cronTime = `${sec} * * * * *`;
+const cronTime = `${sec} ${min} ${hour} * * *`;
 notify(`毎日${hour}時${min}分${sec}秒に投票します`);
 new CronJob(cronTime, () => { vote(); }, undefined, true);
